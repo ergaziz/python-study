2e2f44f957df1a4c856149b049b7e867f3676000
Ergul Azizler <ergaziz@gmail.com>
2017-12-03 15:17:21 +0300

    set continued : algebraic operations

--- 03collections/set.py
+++ 03collections/set.py
@@ -14,6 +14,38 @@ def test_basicset():
     my_empty_set.discard(3)  # this works even if element does not exist
 
 def test_algebraicthings():
-    # 
+    # here we have some people with some stereotypes:
+    blond_hair = {"Jack", "Amelia", "Harry"}
+    print("blond_hair")
+    print(blond_hair)
+    brown_hair = {"John", "Fabri", "Carlos"}
+    print("brown_hair")
+    print(brown_hair)    
+    blue_eyes = {"John", "Amelia", "Cristie"}
+    print("blue_eyes")
+    print(blue_eyes)    
+    brown_eyes = {"Jack", "Harry"}
+    print("brown_eyes")
+    print(brown_eyes)        
+
+    print("blond_hair.union(blue_eyes)")  # union 
+    print(blond_hair.union(blue_eyes))    
+    print("blond_hair.union(blue_eyes) == blue_eyes.union(blond_hair)")
+    print(blond_hair.union(blue_eyes) == blue_eyes.union(blond_hair))  # union is commmutative : true
+
+    print("blond_hair.intersection(blue_eyes)")  # intersection
+    print(blond_hair.intersection(blue_eyes)) # intersection is also commutative
+
+    print("blond_hair.difference(blue_eyes)")  # people with blond hair but not blue eyes
+    print(blond_hair.difference(blue_eyes))  # this is not commutative.
+    
+    print("blond_hair.symmetric_difference(blue_eyes)")  # people with blond hair or blue eyes, not both
+    print(blond_hair.symmetric_difference(blue_eyes))  # this is commutative.
 
 
+    print('{"Harry", "Amelia"}.issubset(blond_hair)')  # means all of the first set is part of the other set.
+    print({"Harry", "Amelia"}.issubset(blond_hair)) 
+    print(blond_hair.issuperset({"Harry", "Amelia"}))  # same as above from other perspective
+
+    print('{"Ali"}.isdisjoint(blond_hair)')  # no members in common
+    print({"Ali"}.isdisjoint(blond_hair))  
